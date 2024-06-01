8f35e9a477ab5cb43380e228400819b279c5f956
pju615 <pju2120@gmail.com>
2024-06-01 20:33:32 +0900

    [Remove] Delete

--- mnu/src/Router.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Notice from "./notice/Notice";
-
-export default function Router() {
-    return (
-        pass
-    )
-}
